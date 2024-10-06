1a877e5b7ae65ce550d57854c206df0795bfb4ee
MelanieBenavente <lavienvert3@gmail.com>
2024-10-06 21:19:16 +0200

    deleted comments

--- app/src/main/java/com/melaniadev/rickandmorty/ui/common/MainActivity.kt
+++ app/src/main/java/com/melaniadev/rickandmorty/ui/common/MainActivity.kt
@@ -6,51 +6,37 @@ import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.melaniadev.rickandmorty.ui.theme.RickAndMortyTheme
+import com.melaniadev.rickandmorty.ui.theme.localCustomColorsPalette
 import dagger.hilt.android.AndroidEntryPoint
 
-/*@AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainActivityContainer)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        supportFragmentManager.beginTransaction().add(R.id.mainActivityContainer, HomeFragment()).commit()
-    }
-}*/
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //todo enableEdgeToEdge()
+        enableEdgeToEdge()
         setContent {
-            MaterialTheme {
+            RickAndMortyTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = Color.Magenta
-
+                    color = localCustomColorsPalette.current.magenta
                 ) {
                     val navigationController = rememberNavController()
 
                     NavHost(
                         navController = navigationController,
-                        startDestination = Routes.HOME.name,
+                        startDestination = NavigationRoute.HOME.name,
                         enterTransition = { EnterTransition.None },
                         exitTransition = { ExitTransition.None }
                     ) {
