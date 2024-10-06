8fc43a4d9122065508f3694e8c36a9f2efd8d7a9
MelanieBenavente <lavienvert3@gmail.com>
2024-10-06 21:09:41 +0200

    Navigation error route and character_detail added to NavGraph

--- app/src/main/java/com/melaniadev/rickandmorty/ui/common/Navigation.kt
+++ app/src/main/java/com/melaniadev/rickandmorty/ui/common/Navigation.kt
@@ -1,51 +1,32 @@
 package com.melaniadev.rickandmorty.ui.common
 
-import androidx.compose.animation.AnimatedContentTransitionScope
-import androidx.compose.animation.core.EaseIn
-import androidx.compose.animation.core.EaseOut
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
-import com.melaniadev.rickandmorty.ui.features.home.CharacterListView
+import com.melaniadev.rickandmorty.ui.features.character.CharacterDetailView
+import com.melaniadev.rickandmorty.ui.features.home.HomeView
 
-enum class Routes {
+enum class NavigationRoute {
     HOME, CHARACTER_DETAIL, BLOCK_ERROR
 }
 
 fun NavGraphBuilder.getNavGraph(navigationController: NavHostController) {
-    composable(Routes.HOME.name) {
-        CharacterListView(navigationController)
+    composable(NavigationRoute.HOME.name) {
+        HomeView(navigationController)
     }
 
-    /*composable(
-        Routes.CHARACTER_DETAIL.name + "/{name}",
-        enterTransition = {
-            fadeIn(
-                animationSpec = tween(
-                    100, easing = LinearEasing
-                )
-            ) + slideIntoContainer(
-                animationSpec = tween(100, easing = EaseIn),
-                towards = AnimatedContentTransitionScope.SlideDirection.Start
-            )
-        },
-        exitTransition = {
-            fadeOut(
-                animationSpec = tween(
-                    300, easing = LinearEasing
-                )
-            ) + slideOutOfContainer(
-                animationSpec = tween(300, easing = EaseOut),
-                towards = AnimatedContentTransitionScope.SlideDirection.End
+    composable(NavigationRoute.CHARACTER_DETAIL.name + "/{id}") {
+        backStackEntry ->
+        backStackEntry.arguments?.getString("id")?.toInt()?.let {
+            CharacterDetailView(
+                navigationController, it
             )
         }
-    ) {
-        backStackEntry -> CharacterDetailScreen(
-            navigationController, backStackEntry.arguments?.getString("name").orEmpty()
+    }
+
+    composable(NavigationRoute.BLOCK_ERROR.name) {
+        ErrorFullScreenView(
+            navigationController
         )
-    }*/
+    }
 }
